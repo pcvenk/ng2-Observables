29938a089ae6353f4172d89b3949aab92a56d589
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-23 15:08:36 +0200

    User Activation/Deactivation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
 import {UserService} from './user/user.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   user1Activated = false;
   user2Activated = false;
+  userSubscription: Subscription;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.userActivated.subscribe(
+    this.userSubscription = this.userService.userActivated.subscribe(
       (id: number) => {
         if (id === 1) {
-          this.user1Activated = true;
+          this.user1Activated = !this.user1Activated;
         } else if (id === 2) {
-          this.user2Activated = true;
+          this.user2Activated = !this.user2Activated;
         }
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 }
