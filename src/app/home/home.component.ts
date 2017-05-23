02e2dd21488a11af696e4db76f027d1ab9783268
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-23 11:28:03 +0200

    Implementing on destroy method for the observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
     const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
+    this.numbersSubscription = myNumbers.subscribe(
       (number: number) => {
         console.log(number);
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.numbersSubscription.unsubscribe();
   }
 
 }
